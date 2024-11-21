0c4fd2edc5e0817cf9307118915882d8c23d0ee3
chuvitvang <152381244+chuvitvang@users.noreply.github.com>
2024-11-21 17:51:33 +0700

    Update giapha.py

--- giapha.py
+++ giapha.py
@@ -1,26 +1,29 @@
 import matplotlib.pyplot as plt
-def gia_pha(n):
-    ketqua = []
-    value = 1
-    for _ in range(n):
-        ketqua.append(value)
-        value *= 2
+def poweroftwo(n):
+    ketqua = []  # Danh sách lưu số lượng thành viên mỗi thế hệ
+    i = 0  # Khởi tạo thế hệ đầu tiên
+    while i <= n:  # Tiếp tục cho đến thế hệ cuối cùng
+        ketqua.append(2**i)  # Tăng trưởng theo hàm mũ
+        i += 1  # Tăng thế hệ
+    print(ketqua)
     return ketqua
-def plot(data):
+def plot(data,so_nam,thehe_tang):
     plt.plot(data, marker='o', linestyle='-', color='b')
-    plt.title("Biểu đồ gia phả theo từng thế hệ")
+    plt.title(f"Biểu đồ gia phả theo từng thế hệ trong {so_nam} năm,với {thehe_tang} năm thêm 1 thế hệ")
     plt.xlabel("Thế hệ")
     plt.ylabel("Số lượng người")
-    # plt.grid(True)
+    plt.grid(True)
     plt.show()
 
-def main():
+def inputfunc():
     so_nam = int(input("Nhập số năm: "))
-    so_the_he = so_nam // int(input("Nhập số năm để sinh ra thế hệ mới (giả sử 30): "))
-    list = gia_pha(so_the_he)
-    print(list)
-    plot(list)
+    thehe_tang = int(input("Nhập số năm để sinh ra thế hệ mới: "))
+    so_the_he = so_nam // thehe_tang
+    return so_the_he,so_nam,thehe_tang
 
+def main():
+    so_the_he, so_nam, thehe_tang = inputfunc()
+    thehe = poweroftwo(so_the_he)
+    plot(thehe,so_nam,thehe_tang)
 
 main()
-
